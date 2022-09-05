bbef9c6643cbecda75c7405f9499992c23fdaa65
Juan Torres <juantorresvelilla@gmail.com>
2022-09-04 23:44:46 -0500

    Primer commit

---
+++ src/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const morgan=require('morgan');
+ 
+//Configuraciones
+app.set('port', process.env.PORT || 3000);
+app.set('json spaces', 2)
+ 
+//Middleware
+app.use(morgan('dev'));
+app.use(express.urlencoded({extended:false}));
+app.use(express.json());
+
+//Routes
+
+app.use(require('./routes/index'));
+app.use('/admin/producto',require('./routes/movies'));
+app.use('/admin/producto/{sku}',require('./routes/movies'));
+
+//Iniciando el servidor
+app.listen(app.get('port'),()=>{
+    console.log(`Server listening on port ${app.get('port')}`);
+});
